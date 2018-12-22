b61e1fcdc9ee0c1eb1e662b256f5ad3a874e666d
Maximo Bautista <max.bautista97@gmail.com>
2018-12-22 05:05:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,12 @@
 #write your code here
 def countdown_with_sleep(number)
-  while number > 0 
+ 
+end
+
+def countdown
+   while number > 0 
      puts "#{number} SECOND(S)!"
      number -= 1
   end
   puts "HAPPY NEW YEAR!"
-end
-
-def countdown
 end
